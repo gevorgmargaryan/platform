b13b75b5c16a4ecd0e3ebffb3c2f7ab61a7884b7
Gevorg Margaryan <gevorg.margaryan@learnship.com>
2020-05-17 21:25:22 +0400

    modified template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,18 @@
 
 @section('body-left')
 
-    <div class="d-sm-flex d-md-block p-3 mt-md-4 w-100 v-center">
-        <a href="#" class="header-toggler d-md-none mr-auto order-first"
-           data-toggle="collapse"
-           data-target="#headerMenuCollapse">
-            <span class="header-toggler-icon icon-menu"></span>
-            <span class="ml-2">@yield('title')</span>
-        </a>
+{{--    <div class="d-sm-flex d-md-block p-3 mt-md-4 w-100 v-center">--}}
+{{--        <a href="#" class="header-toggler d-md-none mr-auto order-first"--}}
+{{--           data-toggle="collapse"--}}
+{{--           data-target="#headerMenuCollapse">--}}
+{{--            <span class="header-toggler-icon icon-menu"></span>--}}
+{{--            <span class="ml-2">@yield('title')</span>--}}
+{{--        </a>--}}
 
-        <a class="header-brand order-last" href="{{route('platform.index')}}">
-            @includeFirst([config('platform.template.header'), 'platform::header'])
-        </a>
-    </div>
+{{--        <a class="header-brand order-last" href="{{route('platform.index')}}">--}}
+{{--            @includeFirst([config('platform.template.header'), 'platform::header'])--}}
+{{--        </a>--}}
+{{--    </div>--}}
 
     <nav class="collapse d-md-block w-100 mb-md-5" id="headerMenuCollapse">
 
@@ -27,17 +27,17 @@
 
     </nav>
 
-    <div class="h-100 w-100 position-relative to-top cursor mt-md-5 divider"
-         data-action="click->layouts--html-load#goToTop"
-         title="{{ __('Go to top') }}">
-        <div class="bottom-left w-100 mb-2 pl-3">
-            <small><i class="icon-arrow-up mr-2"></i> {{ __('Go to top') }}</small>
-        </div>
-    </div>
+{{--    <div class="h-100 w-100 position-relative to-top cursor mt-md-5 divider"--}}
+{{--         data-action="click->layouts--html-load#goToTop"--}}
+{{--         title="{{ __('Go to top') }}">--}}
+{{--        <div class="bottom-left w-100 mb-2 pl-3">--}}
+{{--            <small><i class="icon-arrow-up mr-2"></i> {{ __('Go to top') }}</small>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
-    <div class="p-3 m-b m-t d-none d-lg-block w-100">
-        @includeFirst([config('platform.template.footer'), 'platform::footer'])
-    </div>
+{{--    <div class="p-3 m-b m-t d-none d-lg-block w-100">--}}
+{{--        @includeFirst([config('platform.template.footer'), 'platform::footer'])--}}
+{{--    </div>--}}
 @endsection
 
 @section('body-right')
